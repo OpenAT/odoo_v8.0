922286aa00e114238171c65b5a4abd1636a782fc
Michael Karrer <michaelkarrer81@gmail.com>
2014-10-21 18:46:52 +0200

    [IMP] odoo-tools.sh setup is nearly finished

--- tools/odoo-tools.sh
+++ tools/odoo-tools.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #######################################################################################################################
 
-SOURCE_BRANCH="odoo_v8.0"
+SOURCE_REPO="odoo_v8.0"
 SCRIPT_MODE=$1
 
 # ---------------------------------------------------------
 # SCRIPT START
 # ---------------------------------------------------------
-echo -e "\n----- SCRIPT START odoo-tools.sh -----"
+echo -e "\n===== SCRIPT START odoo-tools.sh ====="
 
 # ----- Set Script Path:
 # "/home/user/bin/foo.sh"
@@ -20,28 +20,35 @@ source /etc/lsb-release
 if [ $DISTRIB_ID = "Ubuntu" ] && [ $DISTRIB_RELEASE = "14.04" ] ; then
     echo -e "OS Version: $DISTRIB_ID $DISTRIB_RELEASE"
 else
-    echo -e "ERROR: This Script only works on Ubuntu 14.04!"
-    exit 2
+    echo -e "ERROR: This Script only works on Ubuntu 14.04!"; exit 2
 fi
 
 # ----- Check script is started as root:
 if [ "$EUID" -ne 0 ]; then
-    echo -e "ERROR: Please run as root!"
-    exit 2
+    echo -e "ERROR: Please run as root!"; exit 2
 fi
 
 # ----- Base Path for all odoo instances:
 BASEPATH="/opt/odoo"
-if cd $BASEPATH ; then
-    echo -e "Changed directory to $BASEPATH"
+if [ -d ${BASEPATH} ]; then
+    echo -e "Directory ${BASEPATH} exists."
 else
-    if mkdir $BASEPATH ; then
-	    echo -e "Created directory $BASEPATH"
-	    cd $BASEPATH
-	    echo -e "Changed directory to $BASEPATH"
+    if mkdir ${BASEPATH} ; then
+	    echo -e "Created directory ${BASEPATH}"
     else
-        echo -e "ERROR: Could not create directory $BASEPATH!"
-	    exit 2
+        echo -e "ERROR: Could not create directory $BASEPATH!"; exit 2
+    fi
+fi
+
+# ----- Base Log Files Path for all odoo instances:
+LOGPATH="/var/log/${SOURCE_REPO}"
+if [ -d ${LOGPATH} ]; then
+    echo -e "Directory ${LOGPATH} exists."
+else
+    if mkdir ${LOGPATH} ; then
+	    echo -e "Created directory ${LOGPATH}"
+    else
+        echo -e "ERROR: Could not create directory ${LOGPATH}!"; exit 2
     fi
 fi
 
@@ -87,11 +94,14 @@ fi
 # $ odoo-tools.sh PREPARE
 # ---------------------------------------------------------
 if [ "$SCRIPT_MODE" = "prepare" ]; then
-    echo -e "\n----- odoo-tools.sh PREPARE -----\n"
+    echo -e "\n-----------------------------------------------------------------------"
+    echo -e " odoo-tools.sh prepare"
+    echo -e "-----------------------------------------------------------------------"
     if [ $# -ne 1 ]; then
         echo -e "ERROR: \"setup-toosl.sh prepare\" takes exactly one argument!"
         exit 2
     fi
+    cd ${BASEPATH}
 
     # ----- Update Server
     echo -e "\n----- Update Server"
@@ -121,7 +131,7 @@ if [ "$SCRIPT_MODE" = "prepare" ]; then
 
     # ----- Install Python Packages
     echo -e "\n----- Install Python Packages"
-    apt-get install python-pip python-dev python-software-properties  -y >> $SETUP_LOG
+    apt-get install python-pip python-dev python-software-properties python-pychart  -y >> $SETUP_LOG
     apt-get install libjpeg-dev libjpeg8-dev libtiff5-dev vflib3-dev pngtools libpng3 -y >> $SETUP_LOG
     apt-get install xvfb xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic -y >> $SETUP_LOG
     apt-get install wkhtmltopdf -y >> $SETUP_LOG
@@ -148,8 +158,8 @@ if [ "$SCRIPT_MODE" = "prepare" ]; then
     chmod ugo=rx $BASEPATH/aeroo.init
     ln -s $BASEPATH/aeroo.init /etc/init.d/aeroo >> $SETUP_LOG
     update-rc.d aeroo defaults >> $SETUP_LOG
-    service aeroo stop >> $SETUP_LOG
-    service aeroo start | tee -a $SETUP_LOG
+    service aeroo stop
+    service aeroo start
     echo -e "----- Install Packages for AerooReports Done"
 
     # ----- Install Packages for Etherpad Lite
@@ -159,9 +169,167 @@ if [ "$SCRIPT_MODE" = "prepare" ]; then
 
 fi
 
+# -----------------------------------------------------------------------
+# $ odoo-tools.sh setup {TARGET_BRANCH} {SUPER_PASSWORD} {DOMAIN_NAME}
+# -----------------------------------------------------------------------
+if [ "$SCRIPT_MODE" = "setup" ]; then
+    echo -e "\n-----------------------------------------------------------------------"
+    echo -e " odoo-tools.sh setup {TARGET_BRANCH} {SUPER_PASSWORD} {DOMAIN_NAME}"
+    echo -e "-----------------------------------------------------------------------"
+    echo -e " You have to run \"odoo-tools.sh prepare\" before setting up your first instance!"
+    if [ $# -ne 4 ]; then
+        echo -e "ERROR: \"setup-toosl.sh prepare\" takes exactly four arguments!"
+        exit 2
+    fi
 
-####### lxml==3.3.5 ?!? * make sure the development packages of libxml2 and libxslt are installed **
-####### No distributions at all found for PyChart==1.39 (from -r /opt/odoo/requirements.txt (line 51))
+    # ----- Set CMD Variables
+    TARGET_BRANCH=$2
+    SUPER_PASSWORD=$3
+    DOMAIN_NAME=$4
+
+    # ----- Create Instance Directory
+    INSTANCE_PATH="${BASEPATH}/${SOURCE_REPO}/${TARGET_BRANCH}"
+    if [ -d ${INSTANCE_PATH} ]; then
+        echo -e "ERROR: ${INSTANCE_PATH} already exists!"; exit 2
+    else
+        if mkdir ${INSTANCE_PATH} ; then
+            echo -e "Created directory ${INSTANCE_PATH}"
+            cd ${INSTANCE_PATH}
+            echo -e "Changed directory to ${INSTANCE_PATH}"
+        else
+            echo -e "ERROR: Could not create directory ${INSTANCE_PATH}!"
+            exit 2
+        fi
+    fi
+
+    # ----- Create Instance Setup Log File
+    INSTANCE_SETUPLOG="$INSTANCE_PATH/instance_setup.log"
+    if [ -w "${INSTANCE_SETUPLOG}" ] ; then
+        echo -e "ERROR: ${INSTANCE_SETUPLOG} already exists!"
+        exit 2
+    else
+        if touch ${INSTANCE_SETUPLOG} ; then
+            echo -e "Instance-Setup-Log: ${INSTANCE_SETUPLOG}. DO NOT MODIFY OR DELETE!"
+        else
+            echo -e "ERROR: Could not create log file ${INSTANCE_SETUPLOG}!"
+            exit 2
+        fi
+    fi
+
+    # ----- Prepare Instance Log Directory and File
+    INSTANCE_LOGFILE="${LOGPATH}/${TARGET_BRANCH}-odoo.log"
+    if [ -w "${INSTANCE_LOGFILE}" ] ; then
+        echo -e "ERROR: ${INSTANCE_LOGFILE} already exists!"
+        exit 2
+    else
+        if touch ${INSTANCE_LOGFILE} ; then
+            echo -e "Instance-Log-File: ${INSTANCE_LOGFILE}."
+        else
+            echo -e "ERROR: Could not create log file ${INSTANCE_LOGFILE}!"
+            exit 2
+        fi
+    fi
+
+    # ---- Set BASEPORT
+    COUNTERFILE=$BASEPATH/INSTANCE.counter
+    if [ -f ${COUNTERFILE} ]; then
+        echo -e "File $COUNTERFILE exists."
+    else
+        if [ touch ${COUNTERFILE} ]; then
+            echo -e "40" > ${COUNTERFILE};
+            echo -e "File ${COUNTERFILE} created."
+        else
+            echo -e "ERROR: Could not create file ${COUNTERFILE}!"
+            exit 2
+        fi
+    fi
+    typeset -i BASEPORT
+    BASEPORT=`cat ${COUNTERFILE}`+1
+    if ${BASEPORT} < 40 ]; then
+        echo echo -e "ERROR: Could not Update ${BASEPORT}!"
+        exit 2
+    fi
+    echo $BASEPORT > ${COUNTERFILE}
+
+    # ----- Basic Variables
+    DBUSER="${TARGET_BRANCH}"
+    DBPW=`tr -cd \#_[:alnum:] < /dev/urandom |  fold -w 8 | head -1`
+    ETHERPADKEY=`tr -cd \#_[:alnum:] < /dev/urandom |  fold -w 16 | head -1`
+
+    # ----- Allow the user to check all arguments:
+    echo -e ""
+    echo -e "\$1 Script Mode                    :  $SCRIPT_MODE" | tee -a $INSTANCE_SETUPLOG
+    echo -e "\$2 Target Branch                  :  $TARGET_BRANCH" | tee -a $INSTANCE_SETUPLOG
+    echo -e "\$3 Super Password                 :  $SUPER_PASSWORD" | tee -a $INSTANCE_SETUPLOG
+    echo -e "\$4 Domain Name                    :  $DOMAIN_NAME" | tee -a $INSTANCE_SETUPLOG
+    echo -e ""
+    echo -e "Instance Setuplog File            :  $INSTANCE_SETUPLOG" | tee -a $INSTANCE_SETUPLOG
+    echo -e ""
+    echo -e "Instance Instance Name            :  $TARGET_BRANCH" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance Base Directory           :  $INSTANCE_PATH" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance Logfile                  :  $INSTANCE_LOGFILE" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance Baseport                 :  $BASEPORT" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance Database User Name       :  $DBUSER" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance Database User Password   :  $DBPW" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance LINUX User               :  $DBUSER" | tee -a $INSTANCE_SETUPLOG
+    echo -e "Instance Etherpad SESSION KEY     :  $ETHERPADKEY" | tee -a $INSTANCE_SETUPLOG
+    echo -e ""
+    echo -e "Would you like to setup a new odoo instance with this settings? ( Y/N ): "; read answer
+    if [ $answer != "Y" ]; then
+        while [ "$answer" != "Y" ]; do
+            if [ "$answer" == "n" ] || [ "$answer" == "N" ]; then
+                echo "SETUP APPORTED: EXITING SCRIPT!"
+                rm -f $INSTANCE_PATH
+                rm $INSTANCE_LOGFILE
+                exit 1
+            fi
+            echo "Please enter Y (Yes) or N (No)"; read answer
+        done
+    fi
+
+    # ----- Create the Linux User and set Rights
+    echo -e "\n----- Create Instance Linux User and Group: ${DBUSER}"
+    useradd -m -s /bin/bash ${DBUSER} | tee -a $INSTANCE_SETUPLOG
+
+    # ----- Create Database User
+    echo -e "\n---- Create postgresql role $DBUSER"
+    sudo su - postgres -c \
+        'psql -a -e -c "CREATE ROLE '${DBUSER}' WITH NOSUPERUSER CREATEDB LOGIN PASSWORD '\'${DBPW}\''"' | tee -a $INSTANCE_SETUPLOG
+
+    # ----- Clone the Github Repo
+    echo -e "\n---- Clone the Github Repo ${SOURCE_REPO}"
+    git clone -b master --depth 1 --single-branch --recurse-submodules \
+        https://github.com/OpenAT/${SOURCE_REPO}.git ${INSTANCE_PATH} | tee -a $INSTANCE_SETUPLOG
+    cd ${INSTANCE_PATH} >> $INSTANCE_SETUPLOG
+    git branch ${TARGET_BRANCH} >> $INSTANCE_SETUPLOG
+    git checkout ${TARGET_BRANCH} >> $INSTANCE_SETUPLOG
+
+    # ----- Set Linux Rights
+    echo -e "\n---- Set Linux Rights ${SOURCE_REPO}"
+    chown ${DBUSER}:${DBUSER} ${INSTANCE_PATH} -Rf >> $INSTANCE_SETUPLOG
+    chown ${DBUSER}:${DBUSER} ${INSTANCE_LOGFILE} >> $INSTANCE_SETUPLOG
+    chmod ug=rw ${INSTANCE_LOGFILE} >> $INSTANCE_SETUPLOG
+    chmod o=r ${INSTANCE_LOGFILE} >> $INSTANCE_SETUPLOG
+
+    # ---- Modify server.conf
+    /bin/sed '{
+        s,"admin_passwd = admin",'"admin_passwd = ${SUPER_PASSWORD}"',g
+        s,"data_dir = data_dir",'"data_dir = ${INSTANCE_PATH}/data_dir"',g
+        s,"db_password = odoo",'"db_password = ${DBPW}"',g
+        s,"db_user = odoo",'"db_user = ${DBUSER}"',g
+        s,"logfile = None",'"logfile = ${INSTANCE_LOGFILE}"',g
+        s,"longpolling_port = 8072",'"longpolling_port = ${BASEPORT}8072"',g
+        s,"xmlrpc_port = 8069",'"xmlrpc_port = ${BASEPORT}8069"',g
+        s,"xmlrpcs_port = 8071",'"xmlrpcs_port = ${BASEPORT}8071"',g
+            }' ${INSTANCE_PATH}/server.conf > ${INSTANCE_PATH}/server-local.conf | tee -a $INSTANCE_SETUPLOG
+
+
+    chmod o=r ${INSTANCE_PATH}/server-local.conf | tee -a $OA_SETUPLOG
+    ln -s ${OA_INSTANCEPATH}/openerp-${OA_INSTANCENAME}.conf /etc/openerp-${OA_INSTANCENAME}.conf | tee -a $OA_SETUPLOG
+
+
+fi
+
 
 # ---------------------------------------------------------
 # Script HELP
