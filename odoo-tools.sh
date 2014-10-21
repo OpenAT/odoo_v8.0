42623ab00ae9c39191756d7cf71a3f0ea72c6cf3
Michael Karrer <michaelkarrer81@gmail.com>
2014-10-21 13:32:27 +0200

    [IMP] odoo-tools.sh removed newlines

--- odoo-tools.sh
+++ odoo-tools.sh
@@ -18,29 +18,29 @@ SCRIPTPATH=$(cd ${0%/*} && pwd -P)
 # ----- Check UBUNTU Version:
 source /etc/lsb-release
 if [ $DISTRIB_ID = "Ubuntu" ] && [ $DISTRIB_RELEASE = "14.04" ] ; then
-    echo -e "\nOS Version: $DISTRIB_ID $DISTRIB_RELEASE"
+    echo -e "OS Version: $DISTRIB_ID $DISTRIB_RELEASE"
 else
-    echo -e "\nERROR: This Script only works on Ubuntu 14.04!"
+    echo -e "ERROR: This Script only works on Ubuntu 14.04!"
     exit 2
 fi
 
 # ----- Check script is started as root:
 if [ "$EUID" -ne 0 ]; then
-    echo -e "\nERROR: Please run as root!"
+    echo -e "ERROR: Please run as root!"
     exit 2
 fi
 
 # ----- Base Path for all odoo instances:
 BASEPATH="/opt/odoo"
 if cd $BASEPATH ; then
-    echo -e "\nChanged directory to $BASEPATH"
+    echo -e "Changed directory to $BASEPATH"
 else
     if mkdir $BASEPATH ; then
-	    echo -e "\nCreated directory $BASEPATH"
+	    echo -e "Created directory $BASEPATH"
 	    cd $BASEPATH
-	    echo -e "\nChanged directory to $BASEPATH"
+	    echo -e "Changed directory to $BASEPATH"
     else
-        echo -e "\nERROR: Could not create directory $BASEPATH!"
+        echo -e "ERROR: Could not create directory $BASEPATH!"
 	    exit 2
     fi
 fi
@@ -48,10 +48,10 @@ fi
 # ----- SETUP LOG-File:
 SETUP_LOG="${BASEPATH}/odoo_setup.log"
 if [ -w "$SETUP_LOG" ] ; then
-    echo -e "\n Setup log file ist at ${SETUP_LOG}. DO NOT MODIFY OR DELETE!"
+    echo -e "Setup log file: ${SETUP_LOG}. DO NOT MODIFY OR DELETE!"
 else
     if touch $SETUP_LOG ; then
-        echo -e "\n Setup log file ist at ${SETUP_LOG}. DO NOT MODIFY OR DELETE!"
+        echo -e "Setup log file ist at ${SETUP_LOG}. DO NOT MODIFY OR DELETE!"
     else
         echo -e "ERROR: Could not create log file ${SETUP_LOG}!"
 	    exit 2
@@ -89,70 +89,70 @@ fi
 if [ "$SCRIPT_MODE" = "prepare" ]; then
     echo -e "\n----- odoo-tools.sh PREPARE -----\n"
     if [ $# -ne 1 ]; then
-        echo -e "\nERROR: \"setup-toosl.sh prepare\" takes exactly one argument!"
+        echo -e "ERROR: \"setup-toosl.sh prepare\" takes exactly one argument!"
         exit 2
     fi
 
     # ----- Update Server
-    echo -e "\n----- Update Server"
+    echo -e "----- Update Server"
     apt-get update >> $SETUP_LOG
     apt-get upgrade -y >> $SETUP_LOG
-    echo -e "\n----- Update Server Done"
+    echo -e "----- Update Server Done"
 
     # ----- Install Basic Packages
-    echo -e "\n----- Install Basic Packages"
+    echo -e "----- Install Basic Packages"
     apt-get install ssh wget sed git git-core gzip curl python libssl-dev build-essential -y >> $SETUP_LOG
-    echo -e "\n----- Install Basic Packages Done"
+    echo -e "----- Install Basic Packages Done"
 
     # ----- Install postgresql
-    echo -e "\n----- Install postgresql"
+    echo -e "----- Install postgresql"
     apt-get install postgresql postgresql-server-dev-9.3 libpq-dev >> $SETUP_LOG
     update-rc.d postgresql defaults >> $SETUP_LOG
     service postgresql restart | tee -a $SETUP_LOG
-    echo -e "\n----- Install postgresql Done"
+    echo -e "----- Install postgresql Done"
 
     # ----- Install nginx
-    echo -e "\n----- Install nginx"
+    echo -e "----- Install nginx"
     apt-get remove apache2 apache2-mpm-event apache2-mpm-prefork apache2-mpm-worker -y >> $SETUP_LOG
     apt-get install nginx -y >> $SETUP_LOG
     update-rc.d nginx defaults >> $SETUP_LOG
     service nginx restart | tee -a $SETUP_LOG
-    echo -e "\n----- Install nginx Done"
+    echo -e "----- Install nginx Done"
 
     # ----- Install Python Packages
-    echo -e "\n----- Install Python Packages"
+    echo -e "----- Install Python Packages"
     apt-get install python-pip python-dev python-software-properties  -y >> $SETUP_LOG
     apt-get install libjpeg-dev libjpeg8-dev libtiff5-dev vflib3-dev pngtools libpng3 -y >> $SETUP_LOG
     apt-get install xvfb xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic -y >> $SETUP_LOG
     apt-get install wkhtmltopdf -y >> $SETUP_LOG
     apt-get install flashplugin-nonfree -y >> $SETUP_LOG
     pip install git+https://github.com/qoda/python-wkhtmltopdf.git >> $SETUP_LOG
-    echo -e "\nInstall requirements.txt"
+    echo -e "Install requirements.txt"
     wget -O- https://raw.githubusercontent.com/OpenAT/odoo_v8.0/master/requirements.txt > $BASEPATH/requirements.txt
     pip install -r $BASEPATH/requirements.txt | tee -a $SETUP_LOG
-    echo -e "\n----- Install Python Packages Done"
+    echo -e "----- Install Python Packages Done"
 
     # ----- Install Packages for AerooReports
-    echo -e "\n----- Install Packages for AerooReports"
+    echo -e "----- Install Packages for AerooReports"
     apt-get install python-genshi python-pyhyphen ure uno-libs3 unoconv libxml2-dev libxslt-dev \
                     libreoffice-core libreoffice-common libreoffice-base libreoffice-base-core \
                     libreoffice-draw libreoffice-calc libreoffice-writer libreoffice-impress \
                     python-cupshelpers hyphen-de hyphen-en-us -y >> $SETUP_LOG
-    echo -e "\nInstall Aeroolib"
+    echo -e "Install Aeroolib"
     git clone --depth 1 --single-branch https://github.com/aeroo/aeroolib.git $BASEPATH/aeroolib >> $SETUP_LOG
     python $BASEPATH/aeroolib/aeroolib/setup.py install | tee -a $SETUP_LOG
-    echo -e "\nInstall Aeroo LibreOffice Service"
+    echo -e "Install Aeroo LibreOffice Service"
     wget -O- https://raw.githubusercontent.com/OpenAT/odoo_v8.0/master/aeroo.init > $BASEPATH/aeroo.init
     ln -s $BASEPATH/aeroo.init /etc/init.d/aeroo.init >> $SETUP_LOG
     update-rc.d aeroo defaults >> $SETUP_LOG
     service aeroo stop >> $SETUP_LOG
     service aeroo start | tee -a $SETUP_LOG
-    echo -e "\n----- Install Packages for AerooReports Done"
+    echo -e "----- Install Packages for AerooReports Done"
 
     # ----- Install Packages for Etherpad Lite
-    echo -e "\nInstall Packages for Etherpad Lite"
+    echo -e "Install Packages for Etherpad Lite"
     apt-get install nodejs abiword -y >> $SETUP_LOG
-    echo -e "\nInstall Packages for Etherpad Lite Done"
+    echo -e "Install Packages for Etherpad Lite Done"
 
 fi
 
@@ -160,11 +160,11 @@ fi
 # ---------------------------------------------------------
 # Script HELP
 # ---------------------------------------------------------
-echo -e "\n\n----- SCRIPT USAGE -----"
-echo -e "\n$ odoo-tools.sh {prepare|setup|deploy|backup|restore} ...\n"
-echo -e "\n$ odoo-tools.sh prepare"
-echo -e "\n$ odoo-tools.sh setup   {TARGET_BRANCH} {SUPER_PASSWORD} {DOMAIN_NAME}"
-echo -e "\n$ odoo-tools.sh deploy  {TARGET_BRANCH} {SUPER_PASSWORD} {DBNAME,DBNAME|all} {ADDON,ADDON}"
-echo -e "\n$ odoo-tools.sh backup  {TARGET_BRANCH} {SUPER_PASSWORD} {DBNAME,DBNAME|all}"
-echo -e "\n$ odoo-tools.sh restore {TARGET_BRANCH} {SUPER_PASSWORD} {DBNAME} {DUMP_TO_RESTORE}"
-echo -e "\n------------------------\n"
+echo -e "\n----- SCRIPT USAGE -----"
+echo -e "$ odoo-tools.sh {prepare|setup|deploy|backup|restore} ...\n"
+echo -e "$ odoo-tools.sh prepare"
+echo -e "$ odoo-tools.sh setup   {TARGET_BRANCH} {SUPER_PASSWORD} {DOMAIN_NAME}"
+echo -e "$ odoo-tools.sh deploy  {TARGET_BRANCH} {SUPER_PASSWORD} {DBNAME,DBNAME|all} {ADDON,ADDON}"
+echo -e "$ odoo-tools.sh backup  {TARGET_BRANCH} {SUPER_PASSWORD} {DBNAME,DBNAME|all}"
+echo -e "$ odoo-tools.sh restore {TARGET_BRANCH} {SUPER_PASSWORD} {DBNAME} {DUMP_TO_RESTORE}"
+echo -e "------------------------"
