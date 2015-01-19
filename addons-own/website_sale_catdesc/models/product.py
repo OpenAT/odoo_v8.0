e4d48db5c568dd971ccff5cb8686fc87e569519b
Michael Karrer <michaelkarrer81@gmail.com>
2015-01-19 18:26:43 +0100

    [ADD] First Version website_sale_catdesc

---
+++ addons-own/website_sale_catdesc/models/product.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013-Today OpenERP SA (<http://www.openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import tools
+from openerp import models, fields
+
+
+# Extend the Product Public Category model with two new fields:
+# desc = Category Description displayed in product list views
+# productinfo = Additional information displayed on product pages for this category
+class product_public_category_desc(models.Model):
+    _inherit = 'product.public.category'
+
+    desc = fields.Html()
+    productinfo = fields.Html()
+
