722c415d597c32407916157c4c81e3cc42d8180c
Michael Karrer <michaelkarrer81@gmail.com>
2015-05-11 14:03:29 +0200

    [IMP] base_config cleanup - please uninstall and install and not just upgrade [ADD] web_dialog_size web_graph_improved web_widget_many2many_tags_multi_selection [UPDATE] odoo addons-thirdparty/server-tools addons-thirdparty/web

--- addons-own/base_config/models/res_config.py
+++ addons-own/base_config/models/res_config.py
@@ -31,85 +31,106 @@ class base_config_settings(osv.osv_memory):
     _name = "base_config.settings"
     _inherit = 'res.config.settings'
     _columns = {
-        'module_base_field_serialized': fields.boolean('Adds fields of type serialized to odoo v8',
+        # Working Third Party Addons
+        'module_base_field_serialized': fields.boolean('base_field_serialized -- Adds fields of type serialized to odoo v8',
                                                        help='Could be needed by older modules - and Aeroo Reports!'
                                                             '\n Installs Module: base_field_serialized'),
-        'module_base_location': fields.boolean('City/Zip auto complete',
+        'module_base_location': fields.boolean('base_location -- City/Zip auto complete',
                                                help='Makes it easier to fill all the Data for Zip, City and State'
                                                     '\n Installs Module: base_location'),
-        'module_base_location_geonames_import': fields.boolean('Donwload City, Zip, State Information',
+        'module_base_location_geonames_import': fields.boolean('base_location_geonames_import -- Donwload City, Zip, State Information',
                                                                help='You could download / update City, Zip and State information!'
-                                                                    '\n Installs Module: base_location'),
-        'module_dbfilter_from_header': fields.boolean('This addon lets you pass a dbfilter as a HTTP header.',
+                                                                    '\n Installs Module: base_location_geonames_import'),
+        'module_dbfilter_from_header': fields.boolean('dbfilter_from_header -- This addon lets you pass a dbfilter as a HTTP header.',
                                                       help='This addon lets you pass a dbfilter as a HTTP header.'
                                                            '\n Installs Module: dbfilter_from_header'),
-        'module_disable_openerp_online': fields.boolean('Disable all Spy from OpenERP SA',
+        'module_disable_openerp_online': fields.boolean('disable_openerp_online -- Disable all Spy from OpenERP SA',
                                                         help='Removes Warning, Online Help and Online Apps'
                                                              '\n Installs Module: disable_openerp_online'),
-        'module_mass_editing': fields.boolean('Mass Editing for any field (set and unset possible)',
+        'module_mass_editing': fields.boolean('mass_editing -- Mass Editing for any field (set and unset possible)',
                                               help='You could create Mass-Editing-Actions for any modell.field in odoo!'
                                                    '\n Installs Module: mass_editing'),
-        'module_web_ckeditor4': fields.boolean('DEPRECATED! CKeditor4 for any html/text field in the odoo backend!',
-                                               help='This is for Version 7 of odoo and is only there for development purposes.'
-                                                    '\n Installs Module: web_ckeditor4'),
-        'module_web_export_view': fields.boolean('Export any Tree-View as Excel Sheet',
+        'module_web_export_view': fields.boolean('web_export_view -- Export any Tree-View as Excel Sheet',
                                                  help='This tool makes the export of tree views much easier since it exports the view as seen on screen!'
                                                       '\n Installs Module: web_export_view'),
-        'module_web_tree_many2one_clickable': fields.boolean('DO NOT INSTALL! Make many2one fields clickabe in tree views',
-                                                             help='You can set a global config option to use this in any tree view - web_tree_many2one_clickable.default True'
-                                                                  '\n Installs Module: web_tree_many2one_clickable'),
-        'module_help_online': fields.boolean('DO NOT INSTALL! Create a help page for any odoo backend view',
-                                             help='This makes it easy to create an inline help for the users of odoo'
-                                                  '\n Installs Module: help_online'),
-        'module_web_recipients_uncheck': fields.boolean('DO NOT INSTALL! Uncheck default receipients of chatter (e-mail)',
-                                                        help='Normaly you can not untick receipients of chatter messages - this makes it possible!'
-                                                             '\n Installs Module: web_recipients_uncheck'),
-        'module_web_m2x_options': fields.boolean('More xml view widget options for many2x fields',
+        'module_web_m2x_options': fields.boolean('web_m2x_options -- More xml view widget options for many2x fields',
                                                  help='Adds options to hide create or create and edit for many2x fields!'
                                                       '\n Installs Module: web_m2x_options'),
-        'module_email_cc_bcc': fields.boolean('DO NOT INSTALL! Add bcc and cc fields to chatter e-mails',
-                                              help='Add bcc and cc fields to chatter e-mai!'
-                                                   '\n Installs Module: email_cc_bcc'),
-        'module_web_filter_tabs': fields.boolean('DO NOT INSTALL! Save Searches as Tabs',
-                                                 help='Save Searches as Tabs'
-                                                      '\n Installs Module: web_filter_tabs'),
-        'module_web_group_expand': fields.boolean('DO NOT INSTALL! Unfold or Fold groups in tree views',
-                                                  help='Unfold or Fold groups in tree views!'
-                                                       '\n Installs Module: web_group_expand'),
-        'module_project_code': fields.boolean('Add a Code to Projects',
-                                              help='Add a code to project.project and make it visible and searchable!'
-                                                   '\n Installs Module: project_code'),
-        'module_website_search': fields.boolean('Global search box for the webpage',
-                                                help='Global search box for the webpage'
-                                                     '\n Installs Module: website_search'),
-        'module_website_blog_private': fields.boolean('BUG! DO NOT LINK! Private (internal) blogs on website',
-                                                      help='Private blogs'
-                                                           '\n Installs Module: website_blog_private'),
-        'module_website_forum_private': fields.boolean('BUG! DO NOT LINK! Private (internal) forums on website',
-                                                       help='Private Forums'
-                                                            '\n Installs Module: website_forum_private'),
-        'module_website_lang_flags': fields.boolean('Language Flags for website lang selector',
-                                                    help='Language Flags instead of just text for lang selector on website'
-                                                         '\n Installs Module: website_lang_flags'),
-        'module_website_crm_extended': fields.boolean('Default sales group for lead from contact formular',
-                                                       help='Adds default sales group an lead creation from website contact formular'
+        'module_website_menu_by_user_status': fields.boolean('website_menu_by_user_status -- Show Webpages depending on the login status',
+                                                 help='Adds options to hide show webpages in Settings - Website - Pages!'
+                                                      '\n Installs Module: website_menu_by_user_status'),
+        'module_website_blog_mgmt': fields.boolean('website_blog_mgmt -- Adds a publish date to blog posts',
+                                                 help='The new publish date field controlls the order and the start date of the blog post!'
+                                                      '\n Installs Module: website_blog_mgmt'),
+        'module_website_sale_collapse_categories': fields.boolean('website_sale_collapse_categories -- collapseabel webshop categories',
+                                                 help='Adds options to make webshop categories collapsible by java script!'
+                                                      '\n Installs Module: website_sale_collapse_categories'),
+        'module_web_dialog_size': fields.boolean('web_dialog_size -- Full width for dialog boxes (nest to close box)',
+                                                 help='Adds button to make dialog popup boxes fill screen width!'
+                                                      '\n Installs Module: web_dialog_size'),
+        'module_web_graph_improved': fields.boolean('web_graph_improved -- compare to measures in graph view',
+                                                 help='compare to measures in graph view!'
+                                                      '\n Installs Module: web_graph_improved'),
+        'module_web_widget_many2many_tags_multi_selection': fields.boolean('web_widget_many2many_tags_multi_selection -- select multible many2many results',
+                                                 help='select multiple results in search-more of many2many fields'
+                                                      '\n Installs Module: many2many_tags_multi_selection'),
+
+        # Addons Own
+        'module_website_crm_extended': fields.boolean('website_crm_extended -- Default sales group for lead from contact formular',
+                                                       help='Adds default sales group for lead creation from website contact formular so that an automatic e-mail can be send'
                                                             '\n Installs Module: website_crm_extended'),
-        'module_payment_frst': fields.boolean('FRST Payment Provider',
+        'module_payment_frst': fields.boolean('payment_frst -- FRST Payment Provider (SEPA - Bankeinzug)',
                                               help='Payment Provider for IBAN and BIC (Bankeinzug)'
                                                    '\n Installs Module: payment_frst'),
-        'module_website_sale_donate': fields.boolean('FRST eCommerce Addons',
+        'module_website_sale_donate': fields.boolean('website_sale_donate -- Shop Extensions for Online Fundraising',
                                                      help='Add arbitrary price, hide amount and other features to website_sale'
                                                           '\n Installs Module: website_sale_donate'),
-        'module_project_basic_extensions': fields.boolean('Basic Project Extensions',
+        'module_project_basic_extensions': fields.boolean('project_basic_extensions -- Basic Project Extensions',
                                                           help='A lot of small tweaks to project, tasks and issues'
                                                           '\n Installs Module: website_sale_donate'),
-        'module_website_highlight_code': fields.boolean('Forum Code Highlighting',
+        'module_website_highlight_code': fields.boolean('website_highlight_code -- Forum Code Highlighting',
                                                       help='Includes highlight.js and add new addons to ckeditor of forum'
                                                            '\n Installs Module: website_sale_catdesc'),
-        'module_mail_follower_control': fields.boolean('Mail Follower Control',
+        'module_mail_follower_control': fields.boolean('mail_follower_control -- Mail Follower Control',
                                                       help='Control the Followers of E-Mails'
                                                            '\n Installs Module: mail_follower_control'),
-        'module_website_sale_catdesc': fields.boolean('Description for Website categories',
-                                                      help='Description for Website categories shown in overview pages'
-                                                           '\n Installs Module: website_sale_catdesc'),
+
+        # Bugy Third Party Addons. Do not link or install (still there as reference)
+        'module_web_ckeditor4': fields.boolean('web_ckeditor4 -- DEPRECATED! CKeditor4 for any html/text field in the odoo backend!',
+                                               help='This is for Version 7 of odoo and is only there for development purposes.'
+                                                    '\n Installs Module: web_ckeditor4'),
+        'module_web_tree_many2one_clickable': fields.boolean('web_tree_many2one_clickable -- Make many2one fields clickabe in tree views',
+                                                             help='You can set a global config option to use this in any tree view - web_tree_many2one_clickable.default True'
+                                                                  '\n Installs Module: web_tree_many2one_clickable'),
+        'module_help_online': fields.boolean('help_online -- Create a help page for any odoo backend view',
+                                             help='This makes it easy to create an inline help for the users of odoo'
+                                                  '\n Installs Module: help_online'),
+        'module_web_recipients_uncheck': fields.boolean('web_recipients_uncheck Uncheck default receipients of chatter (e-mail)',
+                                                        help='Normaly you can not untick receipients of chatter messages - this makes it possible!'
+                                                             '\n Installs Module: web_recipients_uncheck'),
+        'module_email_cc_bcc': fields.boolean('email_cc_bcc -- Add bcc and cc fields to chatter e-mails',
+                                              help='Add bcc and cc fields to chatter e-mai!'
+                                                   '\n Installs Module: email_cc_bcc'),
+        'module_web_filter_tabs': fields.boolean('web_filter_tabs -- Save Searches as Tabs',
+                                                 help='Save Searches as Tabs'
+                                                      '\n Installs Module: web_filter_tabs'),
+        'module_web_group_expand': fields.boolean('web_group_expand -- Unfold or Fold groups in tree views',
+                                                  help='Unfold or Fold groups in tree views!'
+                                                       '\n Installs Module: web_group_expand'),
+        'module_project_code': fields.boolean('project_code -- Add a Code to Projects',
+                                              help='Add a code to project.project and make it visible and searchable!'
+                                                   '\n Installs Module: project_code'),
+        'module_website_search': fields.boolean('website_search -- Global search box for the webpage',
+                                                help='Global search box for the webpage'
+                                                     '\n Installs Module: website_search'),
+        'module_website_blog_private': fields.boolean('website_blog_private -- Private (internal) blogs on website',
+                                                      help='Private blogs'
+                                                           '\n Installs Module: website_blog_private'),
+        'module_website_forum_private': fields.boolean('website_forum_private -- Private (internal) forums on website',
+                                                       help='Private Forums'
+                                                            '\n Installs Module: website_forum_private'),
+        'module_website_lang_flags': fields.boolean('website_lang_flags -- Language Flags for website lang selector',
+                                                    help='Language Flags instead of just text for lang selector on website'
+                                                         '\n Installs Module: website_lang_flags'),
+
     }
