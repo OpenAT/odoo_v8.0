7184db683a4cf37624908b0c4efd9e09571ddff8
Michael Karrer <michaelkarrer81@gmail.com>
2015-07-02 16:31:32 +0200

    [IMP] Add Description Fields to shop product list pages for categories

---
+++ addons-own/website_sale_categories/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2012 OpenERP s.a. (<http://openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "FCOM website_sale_categories",
+    'summary': """Control the display of the shop category navigation menu per category""",
+    'description': """
+
+website_sale_categories
+=======================
+
+- Add Checkbox to public Category: "Hide Category from Navigation"
+- Add Checkbox to public Category: "Start Navigation from this category"
+- Alter Form and List Views
+- Add Descriptions Fields for categories (for product list pages)
+
+    """,
+    'author': "Datadialog - Michael Karrer",
+    'website': "http://www.datadialog.net/",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'website_sale',
+    ],
+    'installable': True,
+    'data': [
+        'templates.xml',
+        'views.xml',
+    ],
+}
