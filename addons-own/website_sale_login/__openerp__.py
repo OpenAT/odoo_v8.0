449ccf849b9d28cc8c6fd0c483ed525fddb37140
Michael Karrer <michaelkarrer81@gmail.com>
2015-08-04 10:10:22 +0200

    [FIX] website_sale_login description markup text fixed

--- addons-own/website_sale_login/__openerp__.py
+++ addons-own/website_sale_login/__openerp__.py
@@ -20,30 +20,31 @@
     'name': "website_sale_login",
     'summary': """FCOM better login pages and user detection by e-mail""",
     'description': """
+website_sale_login
+==================
 
-#website_sale_login
+Check e-mail address for existing partner or user account at checkout.
 
-## shop/checkout and shop/confirm_order
+shop/checkout and shop/confirm_order
+
 - User/Partner detection by e-mail in shop/checkout and shop/confirm_order controllers
 - Show messages for signup or account creation if partner/user found
 - Continue without signup and show messages only once or only at email change if web-user decides to go on without sign in / sign up
-- Quick checkout possible through get variables
-  transfers to /checkout with prefilled form and the product already in basket
+- Quick checkout possible through get variables transfers to /checkout with prefilled form and the product already in basket
   e.g.: /shop/simplecheckout/spenden-3?email=mike@test.com&name=Mike
-- Anonymous quick checkout of products possible through simplecheckout urls with &directpayment=True parameter
-  transfers to /payment with a new res.partner for the SO and the product already in basket
+- Anonymous quick checkout of products possible through simplecheckout urls with &directpayment=True parameter transfers to /payment with a new res.partner for the SO and the product already in basket
   e.g.: /shop/simplecheckout/spenden-3?email=mike2@test.com&shipping_id=0&country_id=13&directpayment=True
 
 TODO:
+
 - Checkbox for Account Creation
-- Checkbox for Newsletter (Maybe an Extra addon or included in website_sale_donate)
-- User Quick Checkout direct to payment (Security Risks have to be considered!)
-  if parnter_id or User_id, email and name in post and all are valid (correct parnter_id or user_id for email and name)
-  then use this partner/user and jump directly to the payment page
-  /shop/simplecheckout/spenden-3?email=mike2@test.com&name=Michael_Karrer&shipping_id=0&country_id=13&partner_id=24
+- Checkbox for Newsletter reception (Maybe an Extra addon or included in website_sale_donate)
+- User Quick Checkout direct to payment (Security Risks have to be considered!) if parnter_id or User_id, email and name in post and all are valid (correct parnter_id or user_id for email and name) then use this partner/user and jump directly to the payment page /shop/simplecheckout/spenden-3?email=mike2@test.com&name=Michael_Karrer&shipping_id=0&country_id=13&partner_id=24
 
 
-HINT: This addon depends on auth_signup for signup or account creation and can be used with auth_doubleoptin also.
+HINT:
+
+This addon depends on auth_signup for signup or account creation and can be used with auth_doubleoptin also.
 
     """,
     'author': "Michael Karrer (michael.karrer@datadialog.net), DataDialog",
