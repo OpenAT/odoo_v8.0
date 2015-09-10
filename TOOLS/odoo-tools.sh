efcb54608d34746f4940634d3879f344aa88f099
Joachim Grubelnik <j.grubelnik@openat.at>
2015-09-10 18:14:46 +0200

    [FIX] another try of restore and db-tools.py

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -1288,15 +1288,15 @@ if [ "$SCRIPT_MODE" = "restore" ]; then
             echo "DATABASE DELETED......"
             # ----- Create a new Database
             #echo -e "\n----- Create Database ${DBNAME}"
-            if ${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${SUPER_PASSWORD} newdb -d ${DBNAME} -p 'adminpw'; then
-                echo -e "Database created!" #| tee -a $DB_SETUPLOG
-            else
-                echo -e "WARNING: Could not create Database ${DBNAME} !\nPlease create it manually!" #| tee -a $DB_SETUPLOG
-            fi
+            #if ${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${SUPER_PASSWORD} newdb -d ${DBNAME} -p 'adminpw'; then
+            #    echo -e "Database created!" #| tee -a $DB_SETUPLOG
+            #else
+            #    echo -e "WARNING: Could not create Database ${DBNAME} !\nPlease create it manually!" #| tee -a $DB_SETUPLOG
+            #fi
 
             # test : access denied --> maybe open connection  ??? echo -e $(${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${DB_PASSWD} "drop" -d ${DBNAME})
 
-            #if ${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${SUPER_PASSWORD} "restore" -d ${DBNAME} -f ${BACKUPFILENAME}; then
+            if ${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${SUPER_PASSWORD} "restore" -d ${DBNAME} -f ${BACKUPFILENAME}; then
             #mit db-tools geht nicht, try manuell alles
             #echo "Unzip ${BACKUPFILENAME}...."
             #unzip ${BACKUPFILENAME}
@@ -1304,8 +1304,8 @@ if [ "$SCRIPT_MODE" = "restore" ]; then
             #echo "DUMPFILE ..... ${DUMPFILE}"
             #echo "Create DB ${DBNAME} with ${BACKUPFILENAME} file.."
             #su - postgres -c "createdb -U ${DB_USER} -T template1 ${DBNAME}"
-            echo "starting restore ....."
-            if $(su - postgres -c "psql -U ${DB_USER} -f ${BACKUPFILENAME} -d ${DBNAME} -h localhost -p ${BASEPORT69}"); then
+            #echo "starting restore ....."
+            #if $(su - postgres -c "psql -f ${BACKUPFILENAME} -d ${DBNAME}"); then
                 echo -e "Datbase restored successfully ..... "
             else
                 echo -e "Error on restoring database ....."
