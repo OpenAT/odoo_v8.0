37191d20c0632188023f44dcf698f678f755ce0d
Joachim Grubelnik <j.grubelnik@openat.at>
2015-09-10 17:27:05 +0200

    [FIX] manualy do the restore, not using db-tools.py

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -1296,7 +1296,15 @@ if [ "$SCRIPT_MODE" = "restore" ]; then
 
             # test : access denied --> maybe open connection  ??? echo -e $(${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${DB_PASSWD} "drop" -d ${DBNAME})
 
-            if ${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${SUPER_PASSWORD} "restore" -d ${DBNAME} -f ${BACKUPFILENAME}; then
+            #if ${INSTANCE_PATH}/TOOLS/db-tools.py -b ${BASEPORT69} -s ${SUPER_PASSWORD} "restore" -d ${DBNAME} -f ${BACKUPFILENAME}; then
+            #mit db-tools geht nicht, try manuell alles
+            echo "Unzip ${BACKUPFILENAME}...."
+            unzip ${BACKUPFILENAME}
+            DUMPFILE=echo ${DATABASE_RUNNING}.zip | cut -d"." -f1,2
+            echo "DUMPFILE ..... ${DUMPFILE}"
+            echo "Create DB ${DBNAME} with ${BACKUPFILENAME} file.."
+            su - postgres -c "createdb -U ${DB_USER} -T template0 ${DBNAME}"
+            if su - postgres -c "psql -c -U ${DB_USER} -f ${INSTANCE_PATH}/${DATABASE_RUNNING}/BACKUP/${DUMPFILE} -d ${DBNAME} -h localhost -p ${BASEPORT69}"; then
                 echo -e "Datbase restored successfully ..... "
             else
                 echo -e "Error on restoring database ....."
