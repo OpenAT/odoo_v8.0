0f686464151944ed420a2f71491040dfef70d5e2
Joachim Grubelnik <j.grubelnik@openat.at>
2015-09-28 13:07:45 +0200

    [ADD] add starting PTD as a service

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -683,7 +683,7 @@ if [ "$SCRIPT_MODE" = "newdb" ]; then
     chmod ugo=rx ${PUSHTODEPLOYINIT}
     ln -s ${PUSHTODEPLOYINIT} /etc/init.d/${PUSHTODEPLOYSERVICENAME}-8${BASEPORT}
     update-rc.d ${PUSHTODEPLOYSERVICENAME}-8${BASEPORT} start 20 2 3 5 . stop 80 0 1 4 6 . | tee -a $DB_SETUPLOG
-    service ${PUSHTODEPLOYSERVICENAME}-8{BASEPORT} start
+    service ${PUSHTODEPLOYSERVICENAME}-8${BASEPORT} start
     #/etc/init.d/${PUSHTODEPLOYSERVICENAME}-8${BASEPORT}
     if git ls-remote ${GITPTDBRANCHNAME} | grep -sw "${GITPTDBRANCHNAME}" 2>&1>/dev/null; then
         echo -e "Cloning Customer Template"
