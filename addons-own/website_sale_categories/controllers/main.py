62841c0adfc8b7d38d6b9a8ce83013570471b6a8
Michael Karrer <michaelkarrer81@gmail.com>
2015-10-07 13:00:30 +0200

    [FIX] website_sale_categories only serch for producst in the current root cat tree or in public categories [FIX] show cart also at cat_hide product list pages [IMP] update.txt

--- addons-own/website_sale_categories/controllers/main.py
+++ addons-own/website_sale_categories/controllers/main.py
@@ -10,14 +10,23 @@ class website_sale_categories(website_sale):
     # List only products in the same cat_root_id tree:
     def _get_search_domain(self, search, category, attrib_values):
         domain = super(website_sale_categories, self)._get_search_domain(search, category, attrib_values)
+        category_obj = request.registry['product.public.category']
         if category:
             # 1.) Find all relevant categories (self, and children with the same cat_root_id)
-            #     HINT: child_of operator will find self and childs! (so not only childs as maybe expected)
-            category_obj = request.registry['product.public.category']
-            child_categories = category_obj.search(request.cr, request.uid, ['&',
-                                                             ('parent_id', 'child_of', int(category)),
-                                                             ('cat_root_id', '=', int(category.cat_root_id))],
+            #     HINT: child_of operator will find self and children! (so not only children as maybe expected)
+            child_categories = category_obj.search(request.cr, request.uid,
+                                                   ['&',
+                                                    ('parent_id', 'child_of', int(category)),
+                                                    ('cat_root_id', '=', int(category.cat_root_id))],
                                                    context=request.context)
             # 2.) Extend the product search domain to only show products which are in one or more of the categories
             domain += [('public_categ_ids', 'in', child_categories)]
+        else:
+            # Only search in non root categories:
+            # if a product has no public_categ_ids
+            # if a product is in one or more public (non root) categories
+            nonroot_categories = category_obj.search(request.cr, request.uid,
+                                                     [('cat_root_id.cat_root', '=', False)],
+                                                     context=request.context)
+            domain += ['|', ('public_categ_ids', '=', False), ('public_categ_ids', 'in', nonroot_categories)]
         return domain
