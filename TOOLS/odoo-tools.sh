6dbcdd482793808de541c45ce9f4d3d5478b3c9a
Joachim Grubelnik <j.grubelnik@openat.at>
2015-10-13 13:45:01 +0200

    [IMP] testing get language

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -1065,7 +1065,7 @@ if [ "$SCRIPT_MODE" = "updatetranslation" ]; then
     echo -e "\n--------------------------------------------------------------------------------------------------------"
     echo -e " $UPDATETRANSLATION"
     echo -e "--------------------------------------------------------------------------------------------------------"
-    if [ $# -ne 7 ]; then
+    if [ $# -ne 6 ]; then
         echo -e "ERROR: \"setup-toosl.sh $SCRIPT_MODE\" takes exactly seven arguments!"
         exit 2
     fi
