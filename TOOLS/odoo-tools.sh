f57dcbb3cb6765db6f5568e30d1d2a72dabe77c8
Joachim Grubelnik <j.grubelnik@openat.at>
2015-10-16 10:53:54 +0200

    [IMP] only specific area

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -1113,9 +1113,10 @@ if [ "$SCRIPT_MODE" = "updatetranslation" ]; then
     #TODO: Backup
     #TODO: GET ISNTALLED LANGUAGES
         #INSTALLEDLOCALE=$(su - postgres -c "psql -d ${DBNAME} -c 'SELECT code, * from res_lang'")
-        INSTALLEDLANG=$(su - postgres -c "psql -d ${DBNAME} -t -c 'SELECT code, iso_code from res_lang'")
+        declare -A INSTALLEDLANG=$(su - postgres -c "psql -d ${DBNAME} -t -c 'SELECT code, iso_code from res_lang'")
+
         #echo "zweidimensional: ${INSTALLEDLANG}"
-        echo "ARRAYLINE: ${INSTALLEDLANG[1]:0:1]}"
+        echo "ARRAYLINE: ${INSTALLEDLANG[1]]}"
         echo $(echo ${INSTALLEDLANG} | sed -n '1p')
         echo "printf"
         printf '%s\n' "${INSTALLEDLANG[@]}"
