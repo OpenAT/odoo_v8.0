98b750f6819d42232330e259ecc317ec2e42ca64
Joachim Grubelnik <j.grubelnik@openat.at>
2015-10-21 10:22:02 +0200

    [FIX] paths and last loaded

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -1064,7 +1064,7 @@ if [ "$SCRIPT_MODE" = "updatetranslation" ]; then
     echo -e "\n--------------------------------------------------------------------------------------------------------"
     echo -e " $UPDATETRANSLATION"
     echo -e "--------------------------------------------------------------------------------------------------------"
-    if [ $# -ne 5 ]; then
+    if [ $# -ge 5 ]; then
         echo -e "ERROR: \"setup-toosl.sh $SCRIPT_MODE\" takes exactly seven arguments!"
         exit 2
     fi
