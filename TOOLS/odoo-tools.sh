c93d2bfc9d4049404815279f484d1abcf19e2c40
Joachim Grubelnik <j.grubelnik@openat.at>
2015-11-24 14:46:28 +0100

    [FIX] prevent multiple backups done in full mode

--- TOOLS/odoo-tools.sh
+++ TOOLS/odoo-tools.sh
@@ -1282,9 +1282,13 @@ if [ "$SCRIPT_MODE" = "backup" ]; then
     for i in "${DATABASES[@]}"; do
         # Check if we are at PAD CLOUD OR ODOO DB
         SINGLEINSTANCE=`echo "${i}" | grep -o ${GREPREGEX}`
-        if ! [[ "${i}" =~ "_pad" ]] || ! [[ "${i}" =~ "_cloud" ]]; then
-            DBFLAG="odoo"
+        if ! [[ "${i}" =~ "_pad" ]]; then
+            DBFLAG="etherpad"
         fi
+        if [[ "${i}" =~ "_cloud" ]]; then
+            DBFLAG="owncloud"
+        fi
+        DBFLAG="odoo"
 
         echo "db: ${i}, dbflag: ${DBFLAG}, single_instance: ${SINGLEINSTANCE}"
 
